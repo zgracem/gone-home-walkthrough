fcd36357debbbf02a44b683923ca0df241f9e0c3
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-10-13 10:21:50 -0600

    add timeline

---
+++ helpers/date_helpers.rb
@@ -0,0 +1,40 @@
+module DateHelpers
+  # Attempts to parse date information into our preferred format,
+  # or returns the input unchanged if it can't.
+
+  def timeline_date(event)
+    date = String.new
+
+    if event["flag"] == "circa"
+      date << "c. "
+    end
+
+    if event["end"]
+      begin
+        start_date = Date.parse(event.date.to_s).strftime('%b. %-d')
+        end_date   = Date.parse(event.end.to_s).strftime('%-d, %Y')
+      rescue
+        start_date = event.date.to_s
+        end_date   = event.end.to_s
+      end
+      date << start_date + "&ndash;" + end_date
+    else
+      begin
+        raw_date = Date.parse(event.date.to_s)
+        date << raw_date.strftime('%b. %-d, %Y')
+      rescue
+        date << event.date.to_s
+      end
+    end
+
+    "#{date} &mdash; #{event.event}"
+  end
+
+  def sort_date(event)
+    if event["sort_date"]
+      Date.parse(event.sort_date.to_s)
+    else
+      Date.parse(event.date.to_s)
+    end
+  end
+end
